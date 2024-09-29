d98e2999d971326faa60ecc566e6761491ec2f63
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-09-29 15:40:16 +0800

    use state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { App } from "./App";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root") as Element);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
