ecd9aff77d211eff29e9b4ce262f11782300f318
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-09-29 16:11:15 +0800

    callback function

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,51 @@ export const App = () => {
   const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  // todosステート（todoのリスト）を更新する関数
+  // イベントリスナー（onsubmit）に渡す関数をコールバック関数という。
+  const handleSubmit = () => {
+    
+    // 何も入力されていなければリターン
+    if (!text) return;
+
+    // 新しいtodoを作成
+    const newTodo: Todo = {
+      value: text,
+    }
+
+    // todosには今のtodoのリストが入っている。それを一度展開して、最初に新しいtodoを追加する。
+    setTodos((todos) => [newTodo, ...todos]);
+
+    // textを操作するメソッドsetTextを介して、textを空にする。
+    setText('');
+  };
+
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}>
         <input 
         type="text" 
         value={text} 
-        onChange={(e) => setText(e.target.value)} />
+        onChange={(e) => handleChange(e)} />
         <input
           type="submit"
           value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={handleSubmit}
         />
       </form>
-      <p>{text}</p>
+      <ul>
+        {todos.map((todo) => {
+          return <li>{todo.value}</li>;
+        })}
+      </ul>
     </div>
   );
 };
